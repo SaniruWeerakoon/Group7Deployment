1f631fc7c306f12758d75af9fcb4f221b1f31d9c
SaniruWeerakoon <CB010868@students.apiit.lk>
2023-06-12 15:04:53 +0530

    hashRouter and .json homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {HashRouter as Router, Route} from "react-router-dom"
 import Navigation from './Navigation';
 import LoginAndSignUp from './LoginAndSignUp';
 import HomePage from "./HomePage";
